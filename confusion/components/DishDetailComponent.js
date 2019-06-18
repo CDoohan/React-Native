369788a23da37a09c4776c018258795cf208c693
CDoohan <pereiradoohan@gmail.com>
2019-06-18 14:33:30 -0300

    Introduzindo React Native Navigation

--- confusion/components/DishDetailComponent.js
+++ confusion/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -23,11 +24,32 @@ function RenderDish(props) {
     }
 }
 
-function DishDetail(props) {
+class DishDetail extends Component {
 
-    return(
-        <RenderDish dish={props.dish} />
-    )
+    constructor(props){
+        super(props)
+
+        this.state = {
+            dishes : DISHES
+        }
+    }
+
+    static navigationOptions = {
+        title   :   'Dish Details'
+    };
+
+    render(){
+        //getPram('nome do parametro usado em navigate', 'valor default caso o parametro seja null')
+        // no caso foi passado navigate('Dishdetail', { dishId : item.id } )
+        const dishId = this.props.navigation.getParam('dishId','');
+
+        return(
+            // o '+' antes e dishId significa que o dishId está sendo transformado de 
+            // string para number, onde no caso é usado para pegar o dishes[dishId]
+            // exemplo: dishes[2], dishes[0], etc.
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        )
+    }
 
 }
 
--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react'
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu        :   { screen : Menu },
+    Dishdetail  :   { screen : DishDetail }
+}, {
+    initialRouteName    :   'Menu',
+    navigationOptions   :   {
+        headerStyle :   {
+            backgroundColor :   '#512DA8'
+        },
+        headerTintColor :   '#fff',
+        headerTitleStyle    :   {
+            color   :   '#fff'
+        }
+    }
+})
 
 export class Main extends Component{
     
@@ -21,10 +38,8 @@ export class Main extends Component{
     
     render() {
         return(
-            <View style={{ flex : 1 }}>
-                <Menu dishes={this.state.dishes} onPress={ (dishId) => this.onDishSelect(dishId) }  />
-
-                <DishDetail dish={this.state.dishes.filter( (dish) => dish.id == this.state.selectedDish )[0] } />  
+            <View style={{ flex : 1, paddingTop : Platform.OS == 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator /> 
             </View>
         );
     }
